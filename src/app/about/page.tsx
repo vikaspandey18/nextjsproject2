c0070bc120f1001ded2b681417ea9ef4d9922419
vikas <vp2841144@gmail.com>
2024-11-20 12:09:28 +0530

    Changin About Us Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
         <h1>Welcome to About Us Page</h1>
         <p className="mt-3">
           <Link href="/" className="bg-blue-400 p-3 rounded-md">
-            Home Page
+            Home
           </Link>
         </p>
       </div>
